9d9f2b12d914ba7786f028eaca4aa49628381793
Isabella Huang <jh84@rice.edu>
2019-11-18 23:00:05 -0600

    add starValue

--- training.py
+++ training.py
@@ -5,6 +5,21 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold
 
 raw_df = pd.read_csv('IMDB-Movie-Data.csv')
+directors_df = pd.read_csv('Top-Directors.csv', encoding = "ISO-8859-1")
+acts_df = pd.read_csv('Top-1000-Actors-Actresses.csv', encoding = "ISO-8859-1")
+
+directors = directors_df['Name'].tolist()
+acts = acts_df['Name'].tolist()
+
+def starV(ppl):
+    val = 0
+    ppl_list = ppl.split(", ")
+    for star in ppl_list:
+        if star in directors or star in acts:
+            val += 1
+    return val
+
+raw_df['StarValue'] = raw_df.apply(lambda x: starV(x['Director'])+ starV(x['Actors']), axis=1)
 
 # remove lines with missing data
 valid_df = raw_df.dropna()
@@ -14,18 +29,21 @@ valid_array = valid_df.values
 
 
 #### Linear Regression ####
-features = valid_df[['Rating', 'Votes', 'Metascore']]
+features = valid_df[['StarValue','Rating', 'Votes', 'Metascore']]
 revenue = valid_df[['Revenue (Millions)']]
 # for data in raw_df.to_numpy():
 #     print(data)
 features = features.to_numpy()
 revenue = revenue.to_numpy()
+
+
 K = 10
 kf = KFold(n_splits=K)
 # print(features)
 
 featuresMean = features.mean(axis=0)
 featuresStd = features.std(axis=0)
+
 # print(featuresStd)
 normFeatures = (features - featuresMean) / featuresStd
 # print(normFeatures)
