5460181afb67226c33cf1e0f88e3531cee47b8f8
LMXCC <516159165@qq.com>
2019-11-18 19:14:27 -0600

    Implementing linear regression.

--- training.py
+++ training.py
@@ -1,11 +1,47 @@
 import numpy as np
 import pandas as pd
+import scipy
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import KFold
 
 raw_df = pd.read_csv('IMDB-Movie-Data.csv')
 raw_array = raw_df.values
 
 # extract x (input) and y (output) from the raw data
 # process x (convert text to quantifiable values, check for missing data, etc.)
+
+#### Linear Regression ####
+features = raw_df[['Rating', 'Votes', 'Metascore']]
+revenue = raw_df[['Revenue (Millions)']]
+for data in raw_df.to_numpy():
+    print(data)
+features = features.to_numpy()
+revenue = revenue.to_numpy()
+K = 10
+kf = KFold(n_splits=K)
+# print(features)
+
+featuresMean = features.mean(axis=0)
+featuresStd = features.std(axis=0)
+# print(featuresStd)
+normFeatures = (features - featuresMean) / featuresStd
+# print(normFeatures)
+squareDistance = 0.0
+for train_index, test_index in kf.split(features):
+    featureTraining, featureTest = normFeatures[train_index], normFeatures[test_index]
+    revenueTraining, revenueTest = revenue[train_index], revenue[test_index]
+    LRModel = LinearRegression().fit(featureTraining, revenueTraining)
+    predictData = LRModel.predict(featureTest)
+    currentDistance = np.sum((revenueTest - predictData)**2)
+    print(currentDistance)
+    squareDistance += currentDistance
+print(squareDistance / K)
+
+
+LRModel = LinearRegression().fit(features, revenue)
+
+
+
 
 # feature selection
 # https://scikit-learn.org/stable/modules/feature_selection.html
