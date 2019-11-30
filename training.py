2bb8f0d00e451ff4815b0dc4444055b6c44dab06
Isabella Huang <jh84@rice.edu>
2019-11-30 12:37:12 -0600

    naive bayes  implemented 75% accuracy; random forest adjusted 70% accuracy

--- training.py
+++ training.py
@@ -16,7 +16,9 @@ directors_df = pd.read_csv('Top-Directors.csv', encoding = "ISO-8859-1")
 acts_df = pd.read_csv('Top-1000-Actors-Actresses.csv', encoding = "ISO-8859-1")
 directors = directors_df['Name'].tolist()
 acts = acts_df['Name'].tolist()
-
+#
+# tmdb = pd.read_csv('train.csv')
+# print (tmdb)
 
 # Create a new column "StarValue" in dataframe based on the director and actors/actresses
 def starV(ppl):
@@ -45,7 +47,13 @@ revenue = revenue.to_numpy()
 
 
 ###############################################################################################
-#                          Linear Regression with KFolds                                      #
+#                                   Feature Selection                                         #
+###############################################################################################
+print (np.median(revenue))
+
+
+###############################################################################################
+#                               Linear Regression with KFolds                                 #
 ###############################################################################################
 # K = 10
 # kf = KFold(n_splits=K)
@@ -82,47 +90,108 @@ revenue = revenue.to_numpy()
 # code from assignments?
 
 # regression models
+###############################################################################################
+#                                       Naive Bayes                                           #
+###############################################################################################
+
+def applyNaiveBayes(X_train, y_train, X_test):
+    # Feature Quantization
+    training_median = np.median(X_train, axis=0)
+    Q_train = X_train - training_median
+    Q_train[Q_train < 0] = 0
+    Q_train[Q_train > 0] = 1
+    Q_test = X_test - training_median
+    Q_test[Q_test < 0] = 0
+    Q_test[Q_test > 0] = 1
+
+    zeros = np.argwhere(y_train == 0)
+    ones = np.argwhere(y_train == 1)
+
+    n_train = 700
+    num_zeros = len(zeros)
+    num_ones = len(ones)
+    Q_train_no = Q_train[zeros, :]
+    Q_train_yes = Q_train[ones, :]
+
+    # P(Feature Value = 0| y = 0)
+    no_no = np.count_nonzero(Q_train_no == 0, axis=0) / num_zeros
+    # P(Feature Value = 0| y = 1)
+    no_yes = np.count_nonzero(Q_train_yes == 0, axis=0) / num_ones
+
+    def predict(arr):
+        no = num_zeros / n_train * np.prod(np.abs(arr - no_no))
+        yes = num_ones / n_train * np.prod(np.abs(arr - no_yes))
+        return int(no < yes)
+    y_predict = np.apply_along_axis(predict, 1, Q_test)
+
+    return y_predict
+
+
+n_train = 700
+
+
+def classify(num):
+    if num < 50:
+        return 0
+    else:
+        return 1
+clf_revenue = np.apply_along_axis(classify, 1, revenue)
+
+perm = np.random.permutation(features.shape[0])
+X_train, y_train = features[perm[:n_train], :], clf_revenue[perm[:n_train]]
+X_test, y_test = features[perm[n_train:], :], clf_revenue[perm[n_train:]]
+# X_train, y_train = features[:n_train, :], clf_revenue[:n_train]
+# X_test, y_test = features[n_train:, :], clf_revenue[n_train:]
+y_hat = applyNaiveBayes(X_train, y_train, X_test)
+print('Naive bayes test error: %g' % (y_test != y_hat).mean())
 
 ###############################################################################################
 #                                       Random Forest                                         #
 ###############################################################################################
 # https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+"""
 n_train = 700
 
 def classify(num):
-    if num < 30: return 0
-    elif num < 50: return 1
-    else: return int(math.floor(math.log(num / 50.0, 2)))
+    if num < 50: return 0
+    else: return 1
+    
 clf_revenue = np.apply_along_axis(classify, 1, revenue)
 
-# X_train, y_train = features[:n_train, :], clf_revenue[:n_train]
-# X_test, y_test = features[n_train:, :], clf_revenue[n_train:]
+X_train, y_train = features[:n_train, :], clf_revenue[:n_train]
+X_test, y_test = features[n_train:, :], clf_revenue[n_train:]
 
-num_trees_range = range(2,20,2)
-oob_score_record = []
-for num_trees in num_trees_range:
-    clf = RandomForestClassifier(n_estimators = num_trees, oob_score= True)
-    # clf.fit(X_train, y_train)
-    clf.fit(features, clf_revenue)
 
-    oob_score_record.append(clf.oob_score_)
-plt.scatter(num_trees_range, oob_score_record)
-plt.show()
-
-idx = np.argmax(oob_score_record)
-num_trees = num_trees_range[idx]
-print ("Using {} trees to train the random forest model has oob score {}".format(num_trees, oob_score_record[idx]))
-# clf = RandomForestClassifier(n_estimators = num_trees, oob_score= True)
-# clf.fit(X_train,y_train)
-# clf.fit(features, clf_revenue)
-# y_predict = clf.predict(X_test)
-# print ('Random forest test error: %g' % (y_predict != y_test).mean())
-# Pclass = (clf_revenue[:, None] == np.arange(2)[None, :]).mean(0)
-# print ('Majority class error: %g' % (clf_revenue != np.argmax(Pclass)).mean())
+"""
+# num_trees_range = range(2,40,2)
+# oob_score_record = []
+# for num_trees in num_trees_range:
+#     clf = RandomForestClassifier(n_estimators = num_trees, oob_score= True)
+#     # clf.fit(X_train, y_train)
+#     clf.fit(features, clf_revenue)
 #
+#     oob_score_record.append(clf.oob_score_)
+# plt.scatter(num_trees_range, oob_score_record)
+# plt.show()
+#
+# idx = np.argmax(oob_score_record)
+# num_trees = num_trees_range[idx]
+# print ("Using {} trees to train the random forest model has oob score {}".format(num_trees, oob_score_record[idx]))
+"""
 
+
+clf = RandomForestClassifier(n_estimators = 32, oob_score= True)
+clf.fit(X_train,y_train)
+# clf.fit(features, clf_revenue)
+y_predict = clf.predict(X_test)
+print ('Random forest test error: %g' % (y_predict != y_test).mean())
+Pclass = (clf_revenue[:, None] == np.arange(2)[None, :]).mean(0)
+print ('Majority class error: %g' % (clf_revenue != np.argmax(Pclass)).mean())
+
+"""
 
 
 
 # k-folds cross-validator
 # https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.KFold.html
+
